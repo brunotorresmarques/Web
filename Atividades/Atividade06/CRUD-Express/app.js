e6e14783c86b2837d705dda80464d9a16d95a983
Bruno Torres <brunotorresepj@gmail.com>
2020-08-31 11:25:03 -0300

    Add Atividade 6

---
+++ Atividades/Atividade06/CRUD-Express/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+var DisciplinaRoute = require('./routes/DisciplinaRoutes')
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/v1/users', users);
+app.use('/disciplinas',DisciplinaRoute)
+
+module.exports = app;
